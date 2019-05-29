9301c898d69195cfb984218ee90b29b526de5597
zicheh <zicheh@localhost.localdomain>
2019-05-28 17:44:12 -0700

    flanger commit 1

---
+++ Lab4p1/stuff/flanger.c
@@ -0,0 +1,35 @@
+// Author:  Zicheng He
+// Date:    May 29th 2019
+// File:    flanger.c
+
+#include "flanger.h"
+#include <math.h>
+
+#define PI 3.14159265
+
+int fla_ii = 0;
+
+void flangerShift(stereoSample*CodecDataIn, stereoSample* CodecDataOut, short* fla_bufL, short* fla_bufR) {
+	short xLeft, xRight, yLeft, yRight;
+	xLeft  = CodecDataIn->Channel[ 0];
+	xRight = CodecDataIn->Channel[ 1];
+	
+	fla_bufL[fla_ii] = xLeft;
+	fla_bufR[fla_ii] = xRight;
+	
+	fla_ii++;
+	
+	int fla_delay = (int) (360 * sin(PI * (fla_ii / 360.0)) + 360);
+	
+	int left_pos = (720 + fla_ii - fla_delay) % 720;
+	int right_pos = (720 + fla_ii - fla_delay) % 720;
+	
+	yLeft = fla_bufL[left_pos];
+	yRight = fla_bufR[right_pos];
+	
+	CodecDataOut->Channel[0] = yLeft;
+	CodecDataOut->Channel[1] = yRight;
+	if (fla_ii > 719) pitchShift_ii = 0;
+	
+	return;
+}
---
+++ Lab4p1/stuff/flanger.h
@@ -0,0 +1,14 @@
+// Author:  Zicheng He
+// Date:    May 29th 2019
+// File:    flanger.h
+
+#ifndef _FLANGER_H_
+#define _FLANGER_H_
+
+#include <stdlib.h>
+#include "../DSP_Config.h"
+#include "stereoSample.h"
+
+void flanger(stereoSample* CodecDataIn, stereoSample* CodecDataOut, short* fla_bufL, short* fla_bufR);
+
+#endif /* _FLANGER_H_ */
